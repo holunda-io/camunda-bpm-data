f80e7798a19fec9a35455ce242ebac84f6587feb
Christian Maschmann <christian.maschmann@holisticon.de>
2019-11-01 15:34:22 +0100

    refactor getTypedValue() and fix assignable-check

--- extension/src/main/java/io/holunda/camunda/bpm/data/adapter/ValueWrapperUtil.java
+++ extension/src/main/java/io/holunda/camunda/bpm/data/adapter/ValueWrapperUtil.java
@@ -10,50 +10,26 @@ import static org.camunda.bpm.engine.variable.Variables.*;
 public class ValueWrapperUtil {
 
   public static <T> TypedValue getTypedValue(Class<T> clazz, Object value, boolean isTransient) {
+    if (!clazz.isAssignableFrom(value.getClass())) {
+      throw new IllegalArgumentException("Couldn't create TypedValue for " + clazz.getSimpleName() + " from value " + value);
+    }
+
     if (String.class.equals(clazz)) {
-      if (value.getClass().isAssignableFrom(clazz)) {
-        return stringValue((String) value, isTransient);
-      } else {
-        throw new IllegalArgumentException("Couldn't create TypedValue for " + clazz.getSimpleName() + " from value " + value);
-      }
+      return stringValue((String) value, isTransient);
     } else if (Boolean.class.equals(clazz)) {
-      if (value.getClass().isAssignableFrom(clazz)) {
-        return booleanValue((Boolean) value, isTransient);
-      } else {
-        throw new IllegalArgumentException("Couldn't create TypedValue for " + clazz.getSimpleName() + " from value " + value);
-      }
+      return booleanValue((Boolean) value, isTransient);
     } else if (Integer.class.equals(clazz)) {
-      if (value.getClass().isAssignableFrom(clazz)) {
-        return integerValue((Integer) value, isTransient);
-      } else {
-        throw new IllegalArgumentException("Couldn't create TypedValue for " + clazz.getSimpleName() + " from value " + value);
-      }
+      return integerValue((Integer) value, isTransient);
     } else if (Short.class.equals(clazz)) {
-      if (value.getClass().isAssignableFrom(clazz)) {
-        return shortValue((Short) value, isTransient);
-      } else {
-        throw new IllegalArgumentException("Couldn't create TypedValue for " + clazz.getSimpleName() + " from value " + value);
-      }
+      return shortValue((Short) value, isTransient);
     } else if (Long.class.equals(clazz)) {
-      if (value.getClass().isAssignableFrom(clazz)) {
-        return longValue((Long) value, isTransient);
-      } else {
-        throw new IllegalArgumentException("Couldn't create TypedValue for " + clazz.getSimpleName() + " from value " + value);
-      }
+      return longValue((Long) value, isTransient);
     } else if (Date.class.equals(clazz)) {
-      if (value.getClass().isAssignableFrom(clazz)) {
-        return dateValue((Date) value, isTransient);
-      } else {
-        throw new IllegalArgumentException("Couldn't create TypedValue for " + clazz.getSimpleName() + " from value " + value);
-      }
+      return dateValue((Date) value, isTransient);
     } else if (Double.class.equals(clazz)) {
-      if (value.getClass().isAssignableFrom(clazz)) {
-        return doubleValue((Double) value, isTransient);
-      } else {
-        throw new IllegalArgumentException("Couldn't create TypedValue for " + clazz.getSimpleName() + " from value " + value);
-      }
+      return doubleValue((Double) value, isTransient);
     } else if (Object.class.equals(clazz)) {
-        return objectValue(value, isTransient).create();
+      return objectValue(value, isTransient).create();
     } else {
       // fallback for null-type
       return untypedValue(value, isTransient);
