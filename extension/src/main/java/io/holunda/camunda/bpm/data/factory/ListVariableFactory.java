1e6bdcfa6c95eb5556f9756de09495dd4290cd6f
Jan Galinski <jan.galinski@holisticon.de>
2020-03-03 12:54:48 +0100

    add equals/hashcode, fixes #22

--- extension/src/main/java/io/holunda/camunda/bpm/data/factory/ListVariableFactory.java
+++ extension/src/main/java/io/holunda/camunda/bpm/data/factory/ListVariableFactory.java
@@ -10,8 +10,10 @@ import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.TaskService;
 import org.camunda.bpm.engine.delegate.VariableScope;
 import org.camunda.bpm.engine.variable.VariableMap;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Variable factory of a base parametrized list type.
@@ -20,32 +22,35 @@ import java.util.List;
  */
 public class ListVariableFactory<T> implements VariableFactory<List<T>> {
 
+  @NotNull
   private final String name;
+
+  @NotNull
   private final Class<T> memberClazz;
 
-  public ListVariableFactory(String name, Class<T> memberClazz) {
+  public ListVariableFactory(@NotNull String name, @NotNull Class<T> memberClazz) {
     this.name = name;
     this.memberClazz = memberClazz;
   }
 
   @Override
   public WriteAdapter<List<T>> on(VariableScope variableScope) {
-    return new ListReadWriteAdapterVariableScope<T>(variableScope, name, memberClazz);
+    return new ListReadWriteAdapterVariableScope<>(variableScope, name, memberClazz);
   }
 
   @Override
   public ReadAdapter<List<T>> from(VariableScope variableScope) {
-    return new ListReadWriteAdapterVariableScope<T>(variableScope, name, memberClazz);
+    return new ListReadWriteAdapterVariableScope<>(variableScope, name, memberClazz);
   }
 
   @Override
   public WriteAdapter<List<T>> on(VariableMap variableMap) {
-    return new ListReadWriteAdapterVariableMap<T>(variableMap, name, memberClazz);
+    return new ListReadWriteAdapterVariableMap<>(variableMap, name, memberClazz);
   }
 
   @Override
   public ReadAdapter<List<T>> from(VariableMap variableMap) {
-    return new ListReadWriteAdapterVariableMap<T>(variableMap, name, memberClazz);
+    return new ListReadWriteAdapterVariableMap<>(variableMap, name, memberClazz);
   }
 
   @Override
@@ -69,11 +74,27 @@ public class ListVariableFactory<T> implements VariableFactory<List<T>> {
   }
 
   @Override
+  @NotNull
   public String getName() {
     return name;
   }
 
+  @NotNull
   public Class<T> getMemberClass() {
     return memberClazz;
+  }
+
+  @Override public boolean equals(Object o) {
+    if (this == o)
+      return true;
+    if (o == null || getClass() != o.getClass())
+      return false;
+    ListVariableFactory<?> that = (ListVariableFactory<?>) o;
+    return name.equals(that.name) &&
+      memberClazz.equals(that.memberClazz);
+  }
+
+  @Override public int hashCode() {
+    return Objects.hash(name, memberClazz);
   }
 }
