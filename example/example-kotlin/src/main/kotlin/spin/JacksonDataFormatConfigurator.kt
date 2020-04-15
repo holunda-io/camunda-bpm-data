dd090db36f764f81add27bafaa7d225613f6de87
Simon Zambrovski <simon.zambrovski@holisticon.de>
2020-04-15 11:11:31 +0200

    remove duplicate configurator for kotlin, fix #39

--- example/example-kotlin/src/main/kotlin/spin/JacksonDataFormatConfigurator.kt
+++ example/example-kotlin/src/main/kotlin/spin/JacksonDataFormatConfigurator.kt
@@ -1,18 +1,16 @@
 package io.holunda.camunda.bpm.data.example.kotlin.spin
 
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.camunda.spin.impl.json.jackson.format.JacksonJsonDataFormat
 import org.camunda.spin.spi.DataFormatConfigurator
 
 class JacksonDataFormatConfigurator : DataFormatConfigurator<JacksonJsonDataFormat> {
 
-  override fun configure(dataFormat: JacksonJsonDataFormat) {
-    val objectMapper = dataFormat.objectMapper
-    objectMapper.registerModule(KotlinModule())
-    objectMapper.registerModule(JavaTimeModule())
-  }
+    override fun configure(dataFormat: JacksonJsonDataFormat) {
+        val objectMapper = dataFormat.objectMapper
+        objectMapper.registerModule(KotlinModule())
+    }
 
-  override fun getDataFormatClass(): Class<JacksonJsonDataFormat> = JacksonJsonDataFormat::class.java
+    override fun getDataFormatClass(): Class<JacksonJsonDataFormat> = JacksonJsonDataFormat::class.java
 
 }
