2745c7ea730f0d0f0d0c904b5dda9456a392cae3
Simon Zambrovski <simon.zambrovski@holisticon.de>
2020-06-24 18:49:56 +0200

    changelog

--- extension/src/test/java/io/holunda/camunda/bpm/data/adapter/ValueWrapperUtilTest.java
+++ extension/src/test/java/io/holunda/camunda/bpm/data/adapter/ValueWrapperUtilTest.java
@@ -32,6 +32,12 @@ public class ValueWrapperUtilTest {
     assertThat(doubleValue.getType()).isExactlyInstanceOf(PrimitiveValueType.DOUBLE.getClass());
     assertThat(doubleValue.getValue()).isEqualTo(Double.MAX_VALUE);
     assertThat(doubleValue.isTransient()).isTrue();
+
+    doubleValue = ValueWrapperUtil.getTypedValue(Double.class, null, true);
+    assertThat(doubleValue).isInstanceOf(DoubleValue.class);
+    assertThat(doubleValue.getType()).isExactlyInstanceOf(PrimitiveValueType.DOUBLE.getClass());
+    assertThat(doubleValue.getValue()).isNull();
+    assertThat(doubleValue.isTransient()).isTrue();
   }
 
   @Test
@@ -47,6 +53,13 @@ public class ValueWrapperUtilTest {
     assertThat(integerValue.getType()).isExactlyInstanceOf(PrimitiveValueType.INTEGER.getClass());
     assertThat(integerValue.getValue()).isEqualTo(Integer.MAX_VALUE);
     assertThat(integerValue.isTransient()).isTrue();
+
+    integerValue = ValueWrapperUtil.getTypedValue(Integer.class, null, true);
+    assertThat(integerValue).isInstanceOf(IntegerValue.class);
+    assertThat(integerValue.getType()).isExactlyInstanceOf(PrimitiveValueType.INTEGER.getClass());
+    assertThat(integerValue.getValue()).isNull();
+    assertThat(integerValue.isTransient()).isTrue();
+
   }
 
   @Test
@@ -64,6 +77,13 @@ public class ValueWrapperUtilTest {
     assertThat(dateValue.getType()).isExactlyInstanceOf(PrimitiveValueType.DATE.getClass());
     assertThat(dateValue.getValue()).isEqualTo(value);
     assertThat(dateValue.isTransient()).isTrue();
+
+    dateValue = ValueWrapperUtil.getTypedValue(Date.class, null, true);
+    assertThat(dateValue).isInstanceOf(DateValue.class);
+    assertThat(dateValue.getType()).isExactlyInstanceOf(PrimitiveValueType.DATE.getClass());
+    assertThat(dateValue.getValue()).isNull();
+    assertThat(dateValue.isTransient()).isTrue();
+
   }
 
   @Test
@@ -79,6 +99,13 @@ public class ValueWrapperUtilTest {
     assertThat(booleanValue.getType()).isExactlyInstanceOf(PrimitiveValueType.BOOLEAN.getClass());
     assertThat(booleanValue.getValue()).isEqualTo(Boolean.FALSE);
     assertThat(booleanValue.isTransient()).isTrue();
+
+    booleanValue = ValueWrapperUtil.getTypedValue(Boolean.class, null, true);
+    assertThat(booleanValue).isInstanceOf(BooleanValue.class);
+    assertThat(booleanValue.getType()).isExactlyInstanceOf(PrimitiveValueType.BOOLEAN.getClass());
+    assertThat(booleanValue.getValue()).isNull();
+    assertThat(booleanValue.isTransient()).isTrue();
+
   }
 
   @Test
@@ -94,6 +121,13 @@ public class ValueWrapperUtilTest {
     assertThat(longValue.getType()).isExactlyInstanceOf(PrimitiveValueType.LONG.getClass());
     assertThat(longValue.getValue()).isEqualTo(Long.MAX_VALUE);
     assertThat(longValue.isTransient()).isTrue();
+
+    longValue = ValueWrapperUtil.getTypedValue(Long.class, null, true);
+    assertThat(longValue).isInstanceOf(LongValue.class);
+    assertThat(longValue.getType()).isExactlyInstanceOf(PrimitiveValueType.LONG.getClass());
+    assertThat(longValue.getValue()).isNull();
+    assertThat(longValue.isTransient()).isTrue();
+
   }
 
   @Test
@@ -109,16 +143,14 @@ public class ValueWrapperUtilTest {
     assertThat(stringValue.getType()).isExactlyInstanceOf(PrimitiveValueType.STRING.getClass());
     assertThat(stringValue.getValue()).isEqualTo("foo");
     assertThat(stringValue.isTransient()).isTrue();
-  }
 
-  @Test
-  public void shouldReturnStringValueWhenValueIsNull() {
-    TypedValue stringValue = ValueWrapperUtil.getTypedValue(String.class, null, false);
+    stringValue = ValueWrapperUtil.getTypedValue(String.class, null, true);
     assertThat(stringValue).isInstanceOf(StringValue.class);
     assertThat(stringValue.getType()).isExactlyInstanceOf(PrimitiveValueType.STRING.getClass());
-    assertThat(stringValue.getValue()).isEqualTo(null);
+    assertThat(stringValue.getValue()).isNull();
+    assertThat(stringValue.isTransient()).isTrue();
   }
-  
+
   @Test
   public void shouldReturnShortValue() {
     TypedValue shortValue = ValueWrapperUtil.getTypedValue(Short.class, Short.MIN_VALUE, false);
@@ -132,6 +164,13 @@ public class ValueWrapperUtilTest {
     assertThat(shortValue.getType()).isExactlyInstanceOf(PrimitiveValueType.SHORT.getClass());
     assertThat(shortValue.getValue()).isEqualTo(Short.MAX_VALUE);
     assertThat(shortValue.isTransient()).isTrue();
+
+    shortValue = ValueWrapperUtil.getTypedValue(Short.class, null, true);
+    assertThat(shortValue).isInstanceOf(ShortValue.class);
+    assertThat(shortValue.getType()).isExactlyInstanceOf(PrimitiveValueType.SHORT.getClass());
+    assertThat(shortValue.getValue()).isNull();
+    assertThat(shortValue.isTransient()).isTrue();
+
   }
 
   @Test
@@ -148,6 +187,13 @@ public class ValueWrapperUtilTest {
     assertThat(objectValue.getType()).isExactlyInstanceOf(PrimitiveValueType.OBJECT.getClass());
     assertThat(objectValue.getValue()).isEqualTo(value);
     assertThat(objectValue.isTransient()).isTrue();
+
+    objectValue = ValueWrapperUtil.getTypedValue(Object.class, null, true);
+    assertThat(objectValue).isInstanceOf(ObjectValue.class);
+    assertThat(objectValue.getType()).isExactlyInstanceOf(PrimitiveValueType.OBJECT.getClass());
+    assertThat(objectValue.getValue()).isNull();
+    assertThat(objectValue.isTransient()).isTrue();
+
   }
 
   @Test
@@ -162,6 +208,12 @@ public class ValueWrapperUtilTest {
     assertThat(untypedValue.getType()).isNull();
     assertThat(untypedValue.getValue()).isEqualTo(value);
     assertThat(untypedValue.isTransient()).isTrue();
+
+    untypedValue = ValueWrapperUtil.getTypedValue(Instant.class, null, true);
+    assertThat(untypedValue.getType()).isInstanceOf(PrimitiveValueType.NULL.getClass());
+    assertThat(untypedValue.getValue()).isNull();
+    assertThat(untypedValue.isTransient()).isTrue();
+
   }
 
   @Test
