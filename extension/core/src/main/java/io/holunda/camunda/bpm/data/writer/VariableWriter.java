78a0b39346abf68bea9dc9b45e2b99553e427d86
Simon Zambrovski <simon.zambrovski@holisticon.de>
2020-06-26 13:29:56 +0200

    fixed imports

--- extension/core/src/main/java/io/holunda/camunda/bpm/data/writer/VariableWriter.java
+++ extension/core/src/main/java/io/holunda/camunda/bpm/data/writer/VariableWriter.java
@@ -1,9 +1,6 @@
 package io.holunda.camunda.bpm.data.writer;
 
 import io.holunda.camunda.bpm.data.adapter.WriteAdapter;
-import io.holunda.camunda.bpm.data.factory.VariableFactory;
-import org.camunda.bpm.engine.variable.VariableMap;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * Inverting calls to {@link WriteAdapter}.
