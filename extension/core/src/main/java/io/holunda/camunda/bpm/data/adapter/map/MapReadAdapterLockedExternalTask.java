f9bf3699573ebea9f16878f20e197ed5a5830f43
Stefan Zilske <stefan.zilske@holisticon.de>
2021-04-23 09:28:48 +0200

    Fix LockedExternalTaskReadAdapter, fixes #109

--- extension/core/src/main/java/io/holunda/camunda/bpm/data/adapter/map/MapReadAdapterLockedExternalTask.java
+++ extension/core/src/main/java/io/holunda/camunda/bpm/data/adapter/map/MapReadAdapterLockedExternalTask.java
@@ -3,6 +3,7 @@ package io.holunda.camunda.bpm.data.adapter.map;
 import io.holunda.camunda.bpm.data.adapter.ReadAdapter;
 import io.holunda.camunda.bpm.data.adapter.WrongVariableTypeException;
 import org.camunda.bpm.engine.externaltask.LockedExternalTask;
+import org.camunda.bpm.engine.variable.Variables;
 
 import java.util.Collections;
 import java.util.Map;
@@ -31,47 +32,47 @@ public class MapReadAdapterLockedExternalTask<K, V> implements ReadAdapter<Map<K
 
   @Override
   public Map<K, V> get() {
-    return null;
+    return getOptional().get();
   }
 
   @Override
   @SuppressWarnings("java:S3655")
-  public Optional<Map<K,V>> getOptional() {
+  public Optional<Map<K, V>> getOptional() {
     return Optional.ofNullable(getOrNull(getValue()));
   }
 
   @Override
-  public Map<K,V> getLocal() {
+  public Map<K, V> getLocal() {
     throw new UnsupportedOperationException("Can't get a local variable on an external task");
   }
 
   @Override
-  public Optional<Map<K,V>> getLocalOptional() {
+  public Optional<Map<K, V>> getLocalOptional() {
     throw new UnsupportedOperationException("Can't get a local variable on an external task");
   }
 
   @Override
-  public Map<K,V> getOrDefault(Map<K,V> defaultValue) {
+  public Map<K, V> getOrDefault(Map<K, V> defaultValue) {
     return getOptional().orElse(defaultValue);
   }
 
   @Override
-  public Map<K,V> getLocalOrDefault(Map<K,V> defaultValue) {
+  public Map<K, V> getLocalOrDefault(Map<K, V> defaultValue) {
     throw new UnsupportedOperationException("Can't get a local variable on an external task");
   }
 
   @Override
-  public Map<K,V> getOrNull() {
+  public Map<K, V> getOrNull() {
     return getOrNull(getValue());
   }
 
   @Override
-  public Map<K,V> getLocalOrNull() {
+  public Map<K, V> getLocalOrNull() {
     throw new UnsupportedOperationException("Can't get a local variable on an external task");
   }
 
   @SuppressWarnings("Duplicates")
-  private Map<K,V> getOrNull(Object value) {
+  private Map<K, V> getOrNull(Object value) {
     if (value == null) {
       return null;
     }
@@ -96,6 +97,8 @@ public class MapReadAdapterLockedExternalTask<K, V> implements ReadAdapter<Map<K
   }
 
   private Object getValue() {
-    return Optional.ofNullable(lockedExternalTask.getVariables()).map(it -> it.get(variableName)).get();
+    return Optional.ofNullable(lockedExternalTask.getVariables())
+      .orElse(Variables.createVariables())
+      .get(variableName);
   }
 }
